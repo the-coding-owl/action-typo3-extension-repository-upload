ae7b04a5e3e877ec70ccf83999f092998256d070
Kevin Ditscheid <kevin@the-coding-owl.de>
2020-03-08 23:23:12 +0100

    [BUGFIX] Quote strings in if conditions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,25 @@
 
 set -eu
 
-if [ -z ${SECRET_USERNAME:-} ]
+if [ -z "${SECRET_USERNAME:-}" ]
 then
   echo "::error::No username provided! Please set SECRET_USERNAME environment variable in your workflow!" >&2
   exit 1
 fi
 
-if [ -z ${SECRET_PASSWORD:-} ]
+if [ -z "${SECRET_PASSWORD:-}" ]
 then
   echo "::error::No password provided! Please set SECRET_PASSWORD environment variable in your workflow!" >&2
   exit 1
 fi
 
-if [ -z ${EXTENSION_KEY:-} ]
+if [ -z "${EXTENSION_KEY:-}" ]
 then
   echo "::error::No extension key provided! Please set EXTENSION_KEY environment variable in your workflow!" >&2
   exit 1
 fi
 
-if [ -z ${UPLOAD_MESSAGE:-} ]
+if [ -z "${UPLOAD_MESSAGE:-}" ]
 then
   echo "::error::No upload message provided! Please set UPLOAD_MESSAGE environment variable in your workflow!" >&2
   exit 1
@@ -31,7 +31,7 @@ echo "::debug::Upload Message: $UPLOAD_MESSAGE"
 echo "::debug::Uploading begin..."
 /app/ter-client upload -u ${SECRET_USERNAME} -p ${SECRET_PASSWORD} -m "${UPLOAD_MESSAGE}" ${EXTENSION_KEY} ${GITHUB_WORKSPACE}
 
-if [! $? -eq 0 ]
+if [ $? -gt 0 ]
 then
   echo "::error::Uploading failed!" >&2
   exit 1
