6cca3affa1e9a2192489d968534f0f9fcb5e45af
Kevin Ditscheid <kevin@the-coding-owl.de>
2020-03-08 22:40:55 +0100

    [TASK] Redirect echo output to stderr

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,25 +4,25 @@ set -eu
 
 [ -z $SECRET_USERNAME ]
 then:
-  echo "No username provided! Please set SECRET_USERNAME environment variable in your workflow!"
+  echo "No username provided! Please set SECRET_USERNAME environment variable in your workflow!" >&2
   exit 1
 fi
 
 [ -z $SECRET_PASSWORD ]
 then:
-  echo "No password provided! Please set SECRET_PASSWORD environment variable in your workflow!"
+  echo "No password provided! Please set SECRET_PASSWORD environment variable in your workflow!" >&2
   exit 1
 fi
 
 [ -z $EXTENSION_KEY ]
 then:
-  echo "No extension key provided! Please set EXTENSION_KEY environment variable in your workflow!"
+  echo "No extension key provided! Please set EXTENSION_KEY environment variable in your workflow!" >&2
   exit 1
 fi
 
 [ -z $UPLOAD_MESSAGE ]
 then:
-  echo "No upload message provided! Please set UPLOAD_MESSAGE environment variable in your workflow!"
+  echo "No upload message provided! Please set UPLOAD_MESSAGE environment variable in your workflow!" >&2
   exit 1
 fi
 
@@ -30,5 +30,12 @@ echo "Extension: $EXTENSION_KEY"
 echo "Upload Message: $UPLOAD_MESSAGE"
 echo "Uploading begin..."
 /app/ter-client upload -u ${SECRET_USERNAME} -p ${SECRET_PASSWORD} -m "${UPLOAD_MESSAGE}" ${EXTENSION_KEY} ${GITHUB_WORKSPACE}
+
+[! $? -eq 0 ]
+then:
+  echo "Uploading failed!" >&2
+  exit 1
+fi
+
 echo "Finished"
 exit 0
